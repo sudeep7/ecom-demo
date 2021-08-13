51e6d045ecdfc59ebf3eec2b9681a3f3d043763d
sudeep-dhonde <sudeep.dhonde@hpe.com>
2021-08-13 09:33:55 +0530

    initial commit: project structure

---
+++ src/test/java/com/mojo/ecom/EcomDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mojo.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
